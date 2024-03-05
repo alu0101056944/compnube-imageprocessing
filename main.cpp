864e018369a0cd7f590f71449b5109aba0b872f3
alu0101056944@ull.edu.es <alu0101056944@ull.edu.es>
2024-03-05 17:44:24 +0000

    Save

--- main.cpp
+++ main.cpp
@@ -35,8 +35,10 @@ int main(int argc, char** argv) {
 
       const int kMinimumX = j - kRadius;
       const int kMinimumY = i - kRadius;
-      for (int ii = j - kRadius; ii < std::min(image.rows, j + kRadius); ++ii) {
-        for (int jj = j - kRadius; jj < std::min(image.cols, j + kRadius); ++jj) {
+      for (int ii = std::max(0, i - kRadius);
+               ii < std::min(image.rows, i + kRadius); ++ii) {
+        for (int jj = std::max(0, j - kRadius);
+                 jj < std::min(image.cols, j + kRadius); ++jj) {
           cv::Vec3b& pixelNeighbor = image.at<cv::Vec3b>(ii, jj);
 
           const double kR = pixelNeighbor.val[2];
