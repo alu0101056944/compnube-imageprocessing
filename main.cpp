fdfd2c992d58df8422cd12036e970ad841bc7e83
alu0101056944@ull.edu.es <alu0101056944@ull.edu.es>
2024-03-05 17:27:36 +0000

    Save

--- main.cpp
+++ main.cpp
@@ -19,77 +19,7 @@ int main(int argc, char** argv) {
     return -1;
   }
 
-  const int kIntensityLevels = 6;
-
-  const int kRadius = 5;
-  for (int i = 0; i < image.rows; ++i) {
-    for (int j = 0; j < image.cols; ++j) {
-      cv::Vec3b& pixel = image.at<cv::Vec3b>(i, j);
-
-      int maximumIntensity = -1;
-
-      std::unordered_map<int, int> colorTotalsR;
-      std::unordered_map<int, int> colorTotalsG;
-      std::unordered_map<int, int> colorTotalsB;
-      std::unordered_map<int, int> intensityCount;
-
-      const int kMinimumX = j - kRadius;
-      const int kMinimumY = i - kRadius;
-      for (int ii = j - kRadius; ii < std::min(image.rows, j + kRadius); ++ii) {
-        for (int jj = j - kRadius; jj < std::min(image.cols, j + kRadius); ++jj) {
-          cv::Vec3b& pixelNeighbor = image.at<cv::Vec3b>(ii, jj);
-
-          const double kR = pixelNeighbor.val[2];
-          const double kG = pixelNeighbor.val[1];
-          const double kB = pixelNeighbor.val[0];
-
-          // do the calculation of how many intensities are there
-          const int kIntensity = (((kR + kG + kB) / 3) * kIntensityLevels) / 255.0f;
-
-          auto it = intensityCount.find(kIntensity);
-          if (it != intensityCount.end()) {
-            ++intensityCount[kIntensity];
-          } else {
-            intensityCount[kIntensity] = 1;
-          }
-
-          if (maximumIntensity == -1 || maximumIntensity < kIntensity) {
-            maximumIntensity = kIntensity;
-          }
-
-          auto it2 = colorTotalsR.find(kIntensity);
-          if (it2 != colorTotalsR.end()) {
-            colorTotalsR[kIntensity] += kR;
-          } else {
-            colorTotalsR[kIntensity] = 0;
-          }
-          auto it3 = colorTotalsG.find(kIntensity);
-          if (it3 != colorTotalsG.end()) {
-            colorTotalsG[kIntensity] += kG;
-          } else {
-            colorTotalsG[kIntensity] = 0;
-          }
-          auto it4 = colorTotalsB.find(kIntensity);
-          if (it4 != colorTotalsB.end()) {
-            colorTotalsB[kIntensity] += kB;
-          } else {
-            colorTotalsB[kIntensity] = 0;
-          }
-        }
-      }
-
-      const int kRFinal =
-          colorTotalsR[maximumIntensity] / intensityCount[maximumIntensity];
-      const int kGFinal =
-          colorTotalsG[maximumIntensity] / intensityCount[maximumIntensity];
-      const int kBFinal =
-          colorTotalsB[maximumIntensity] / intensityCount[maximumIntensity];
-
-      pixel.val[2] = kRFinal;
-      pixel.val[1] = kGFinal;
-      pixel.val[0] = kBFinal;
-    }
-  }
+  cv::Vec3b& pixel = image.at<cv::Vec3b>(3000, 3000);
 
   cv::imwrite("docs/f1_processed.jpg", image);
 
