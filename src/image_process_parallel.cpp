19df45920f431b9bfdbeb036bcf3bf625c3902ba
alu0101056944@ull.edu.es <alu0101056944@ull.edu.es>
2024-03-10 23:47:57 +0000

    Save

--- src/image_process_parallel.cpp
+++ src/image_process_parallel.cpp
@@ -10,6 +10,7 @@
   const int kIntensityLevels = 40;
 
   const int kRadius = 3;
+#pragma omp parallel for default(private)
   for (int i = 0; i < image.rows; ++i) {
     for (int j = 0; j < image.cols; ++j) {
       cv::Vec3b& outputPixel = outputImage.at<cv::Vec3b>(i, j);
