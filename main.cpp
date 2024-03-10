8b50729b22c69afa9e8a9a922909fc1bc602e436
alu0101056944@ull.edu.es <alu0101056944@ull.edu.es>
2024-03-10 19:31:40 +0000

    Save

--- main.cpp
+++ main.cpp
@@ -39,7 +39,7 @@ int main(int argc, char** argv) {
 
   const int kIntensityLevels = 4;
 
-  const int kRadius = 5;
+  const int kRadius = 3;
   for (int i = 0; i < image.rows; ++i) {
     for (int j = 0; j < image.cols; ++j) {
       cv::Vec3b& outputPixel = outputImage.at<cv::Vec3b>(i, j);
