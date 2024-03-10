b43f6a70e25f673d0a0c6b8a1290f9627b2b4abc
alu0101056944@ull.edu.es <alu0101056944@ull.edu.es>
2024-03-10 19:12:14 +0000

    Save

--- main.cpp
+++ main.cpp
@@ -37,7 +37,7 @@ int main(int argc, char** argv) {
 
   cv::Mat outputImage = image.clone();
 
-  const int kIntensityLevels = 20;
+  const int kIntensityLevels = 4;
 
   const int kRadius = 10;
   for (int i = 0; i < image.rows; ++i) {
