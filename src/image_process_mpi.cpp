37a1a1bddca3eaf07efa5a11832787df144e1a34
alu0101056944@ull.edu.es <alu0101056944@ull.edu.es>
2024-04-09 12:28:16 +0100

    Save

--- src/image_process_mpi.cpp
+++ src/image_process_mpi.cpp
@@ -13,8 +13,7 @@
     const std::vector<double>& full, int startPos, int endPos) {
   std::vector<double> outputPixels(endPos - startPos + 1); // resize
 
-  srand(time(0));
-  for (int i = startPos; i < std::min((int)full.size(), endPos + 1); ++i) {
+  for (int i = 0; i < outputPixels.size(); ++i) {
     outputPixels[i] = -7;
   }
 
