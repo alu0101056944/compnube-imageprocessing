f1fbbde3a40cef00a1fda95c9295b3df4702bace
alu0101056944@ull.edu.es <alu0101056944@ull.edu.es>
2024-04-09 11:57:56 +0100

    Save

--- main_mpi.cpp
+++ main_mpi.cpp
@@ -47,6 +47,18 @@ int main(int argc, char** argv) {
     std::vector<double> chunk =
         getProcessedImageParallelMPI(fullMatrix, kStartPixel, kEndPixel);
 
+    if (rank === 0) {
+      {
+        volatile int i = 0;
+        char hostname[256];
+        gethostname(hostname, sizeof(hostname));
+        printf("PID %d on %s ready for attach\n", getpid(), hostname);
+        fflush(stdout);
+        while (0 == i)
+            sleep(5);
+      }
+    }
+
     if (rank == 3) {
       std::cout << "size: " << chunk.size() << std::endl;
       std::cout << std::endl << "|  chunk: ";
