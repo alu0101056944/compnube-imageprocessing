6f9def82577b9446ba57e2e1cbf554381fa90819
alu0101056944@ull.edu.es <alu0101056944@ull.edu.es>
2024-04-15 13:31:49 +0100

    Save

--- main_mpi.cpp
+++ main_mpi.cpp
@@ -69,6 +69,16 @@ int main(int argc, char** argv) {
     return -1;
   }
 
+  {
+      volatile int i = 0;
+      char hostname[256];
+      gethostname(hostname, sizeof(hostname));
+      printf("PID %d on %s ready for attach\n", getpid(), hostname);
+      fflush(stdout);
+      while (0 == i)
+          sleep(5);
+    }
+
   printExecutionTime(image, rank, size);
   writeImage(image, inputPath, rank, size);
 
