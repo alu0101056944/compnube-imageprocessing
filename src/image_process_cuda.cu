0257327ad37be5d0dead874aaab1affb40c721cb
alu0101056944 <alu0101056944>
2024-07-08 06:45:45 +0100

    Changing to suggestions.

--- src/image_process_cuda.cu
+++ src/image_process_cuda.cu
@@ -12,7 +12,8 @@
 #include <opencv2/core/cuda.hpp>
 #include <iostream>
 
-__global__ void _getImageChunk(uchar* originalData, uchar* pixelData, int rows, int cols) {
+__global__ void _getImageChunk(uchar* originalData, uchar* pixelData, int rows, int cols,
+    int step) {
   const int kIntensityLevels = 20;
   const int kRadius = 5;
 
@@ -24,161 +25,69 @@ __global__ void _getImageChunk(uchar* originalData, uchar* pixelData, int rows, 
     const int kRow = floorf(kPixelIndex / cols);
     const int kColumn = kPixelIndex % cols;
 
-    pixelData[kRow * 3 * cols + kColumn * 3 + 2] = 55;
-    pixelData[kRow * 3 * cols + kColumn * 3 + 1] = 55;
-    pixelData[kRow * 3 * cols + kColumn * 3] = 55;
+    int maximumIntensity = -1;
 
-    // int maximumIntensity = -1;
+    // 255 because there won't be more than 255 intensities
+    int colorTotalsR[255] = {0};
+    int colorTotalsG[255] = {0};
+    int colorTotalsB[255] = {0};
+    int intensityCount[255] = {0};
 
-    // // 255 because there won't be more than 255 intensities
-    // int colorTotalsR[255] = {0};
-    // int colorTotalsG[255] = {0};
-    // int colorTotalsB[255] = {0};
-    // int intensityCount[255] = {0};
+    for (int ii = max(0, kRow - kRadius);
+        ii < min(rows, kRow + kRadius); ++ii) {
+      for (int jj = max(0, kColumn - kRadius);
+            jj < min(cols, kColumn + kRadius); ++jj) {
+        const double kR = originalData[ii * step + jj * 3 + 2];
+        const double kG = originalData[ii * step + jj * 3 + 1];
+        const double kB = originalData[ii * step + jj * 3];
 
-    // for (int ii = max(0, kRow - kRadius);
-    //     ii < min(rows, kRow + kRadius); ++ii) {
-    //   for (int jj = max(0, kColumn - kRadius);
-    //         jj < min(cols, kColumn + kRadius); ++jj) {
-    //     const int kRow2 = floorf(ii / cols);
-    //     const int kColumn2 = ii % cols;
-          
-    //     const double kR = originalData[kRow2 * cols + kColumn2 + 2];
-    //     const double kG = originalData[kRow2 * cols + kColumn2 + 1];
-    //     const double kB = originalData[kRow2 * cols + kColumn2];
+        int intensity = (((kR + kG + kB) / 3) * kIntensityLevels) / 255.0f;
+        if (intensity > 255) {
+          intensity = 255;
+        }
 
-    //     int intensity = (((kR + kG + kB) / 3) * kIntensityLevels) / 255.0f;
-    //     if (intensity > 255) {
-    //       intensity = 255;
-    //     }
+        ++intensityCount[intensity];
 
-    //     ++intensityCount[intensity];
+        if (maximumIntensity == -1 || maximumIntensity < intensity) {
+          maximumIntensity = intensity;
+        }
 
-    //     if (maximumIntensity == -1 || maximumIntensity < intensity) {
-    //       maximumIntensity = intensity;
-    //     }
+        colorTotalsR[intensity] += kR;
+        colorTotalsG[intensity] += kG;
+        colorTotalsB[intensity] += kB;
+      }
+    }
 
-    //     colorTotalsR[intensity] += kR;
-    //     colorTotalsG[intensity] += kG;
-    //     colorTotalsB[intensity] += kB;
-    //   }
-    // }
+    const int kRFinal =
+        colorTotalsR[maximumIntensity] / intensityCount[maximumIntensity];
+    const int kGFinal =
+        colorTotalsG[maximumIntensity] / intensityCount[maximumIntensity];
+    const int kBFinal =
+        colorTotalsB[maximumIntensity] / intensityCount[maximumIntensity];
 
-    // const int kRFinal =
-    //     colorTotalsR[maximumIntensity] / intensityCount[maximumIntensity];
-    // const int kGFinal =
-    //     colorTotalsG[maximumIntensity] / intensityCount[maximumIntensity];
-    // const int kBFinal =
-    //     colorTotalsB[maximumIntensity] / intensityCount[maximumIntensity];
-
-    // pixelData[kRow * 3 * cols + kColumn * 3 + 2] = kRFinal;
-    // pixelData[kRow * 3 * cols + kColumn * 3 + 1] = kGFinal;
-    // pixelData[kRow * 3 * cols + kColumn * 3] = kBFinal;
+    const int kRowFull = floorf(kPixelIndex / step);
+    pixelData[kRowFull * step + kColumn * 3 + 2] = kRFinal;
+    pixelData[kRowFull * step + kColumn * 3 + 1] = kGFinal;
+    pixelData[kRowFull * step + kColumn * 3] = kBFinal;
   }
 }
 
 cv::Mat getProcessedImageParallelCUDA(const cv::Mat& image) {
   cv::Mat imageCopy;
   image.copyTo(imageCopy);
-
-  const int kRow = 0;
-  const int kCol = 0;
-
-  cv::Vec3b& pixel = imageCopy.at<cv::Vec3b>(0, 0);
-  const double kR = pixel.val[2];
-  const double kG = pixel.val[1];
-  const double kB = pixel.val[0];
-
-  cv::Vec3b& pixel2 = imageCopy.at<cv::Vec3b>(1, 0);
-  const double kR2 = pixel2.val[2];
-  const double kG2 = pixel2.val[1];
-  const double kB2 = pixel2.val[0];
-
-  cv::Vec3b& pixel3 = imageCopy.at<cv::Vec3b>(0, 1);
-  const double kR3 = pixel3.val[2];
-  const double kG3 = pixel3.val[1];
-  const double kB3 = pixel3.val[0];
-
-  cv::Vec3b& pixel4 = imageCopy.at<cv::Vec3b>(1, 1);
-  const double kR4 = pixel4.val[2];
-  const double kG4 = pixel4.val[1];
-  const double kB4 = pixel4.val[0];
-
-  printf("originalimage: (0:%f,1:%f,2:%f),(0:%f,1:%f,2:%f),(0:%f,1:%f,2:%f),(0:%f,1:%f,2:%f)\n",
-    kR,
-    kB,
-    kG,
-    kR2,
-    kB2,
-    kG2,
-    kR3,
-    kB3,
-    kG3,
-    kR4,
-    kB4,
-    kG4
-  );
-
-  printf("original: (0:%d, 1: %d, 2: %d)\n",
-    imageCopy.data[6],
-    imageCopy.data[7],
-    imageCopy.data[8]
-  );
-
-  pixel.val[2] = 255;
-  pixel.val[1] = 0;
-  pixel.val[0] = 0;
-
-  // imageCopy.data[kRow * 3 + kCol * 3] = 0;
-  // imageCopy.data[kRow * 3 + kCol * 3 + 1] = 0;
-  // imageCopy.data[kRow * 3 + kCol * 3 + 2] = 255;
-
-  const double kRa = pixel.val[2];
-  const double kGa = pixel.val[1];
-  const double kBa = pixel.val[0];
-  const double kR2a = pixel2.val[2];
-  const double kG2a = pixel2.val[1];
-  const double kB2a = pixel2.val[0];
-  const double kR3a = pixel3.val[2];
-  const double kG3a = pixel3.val[1];
-  const double kB3a = pixel3.val[0];
-  const double kR4a = pixel4.val[2];
-  const double kG4a = pixel4.val[1];
-  const double kB4a = pixel4.val[0];
-
-  printf("originalimage(after): (0:%f,1:%f,2:%f),(0:%f,1:%f,2:%f),(0:%f,1:%f,2:%f),(0:%f,1:%f,2:%f)\n",
-    kRa,
-    kBa,
-    kGa,
-    kR2a,
-    kB2a,
-    kG2a,
-    kR3a,
-    kB3a,
-    kG3a,
-    kR4a,
-    kB4a,
-    kG4a
-  );
-
-  printf("changed: (0:%d, 1: %d, 2: %d)\n",
-      imageCopy.data[6],
-      imageCopy.data[7],
-      imageCopy.data[8]
-    );
+  cv::cuda::GpuMat originalImageInGPU(image.rows, image.cols, CV_8UC3);
+  originalImageInGPU.upload(imageCopy);
 
   // use step in the code
+  cv::cuda::GpuMat newGpuImage(image.rows, image.cols, CV_8UC3);
+  newGpuImage.upload(imageCopy);
 
-  // cv::cuda::GpuMat newGpuImage(image.rows, image.cols, CV_8UC3);
-  // newGpuImage.upload(imageCopy);
+  dim3 threadsPerBlock(32, 32);
+  dim3 gridSize((image.cols + threadsPerBlock.x - 1) / threadsPerBlock.x, 
+          (image.rows + threadsPerBlock.y - 1) / threadsPerBlock.y);
+  _getImageChunk<<<gridSize, threadsPerBlock>>>(originalImageInGPU.data,
+      newGpuImage.data, image.rows, image.cols, newGpuImage.step);
 
-  // const int kThreadAmountPerBlock = 32 * 32;
-  // const int kBlockAmount =
-  //     ceil(((float)image.rows * (float)image.cols) / kThreadAmountPerBlock);
-  // dim3 threadsPerBlock(32, 32);
-  // _getImageChunk<<<kBlockAmount, threadsPerBlock>>>(imageCopy.data,
-  //     newGpuImage.data, image.rows, image.cols);
-
-  // newGpuImage.download(imageCopy);
+  newGpuImage.download(imageCopy);
   return imageCopy;
 }
